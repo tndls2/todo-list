de4f396737e9818d86fa7fd80023a3d165482404
suin park <tndls3026@ajou.ac.kr>
2024-09-16 18:29:04 +0900

    [feat/accounts] create User model, User Manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, user_name, password=None, **extra_fields):
+        """
+        Create and return a regular user with a username and password.
+        """
+        if not user_name:
+            raise ValueError('The User must have a user_name.')
+        if self.filter(user_name=user_name).exists():  # user_name 중복 체크
+            raise ValueError('A user with this user_name already exists.')
+
+        user = self.model(user_name=user_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, user_name, password=None, **extra_fields):
+        """
+        Create and return a superuser with a username and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(user_name, password, **extra_fields)
+
+
+class User(models.Model):
+    # Django 내장 User 모델 및 기능을 사용하지 않고 직접 구현
+    user_name = models.CharField(max_length=150, unique=True)
+    password = models.CharField(_("password"), max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'user_name'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.user_name
+
+    def set_password(self, raw_password):
+        """
+        Set the password for this user. The raw password is hashed and stored in the database.
+        """
+        self.password = make_password(raw_password)
+        self.save(update_fields=['password'])
+
+    def check_password(self, raw_password):
+        """
+        Check if the raw_password matches the hashed password stored in the database.
+        """
+        return check_password(raw_password, self.password)
+
+    def save(self, *args, **kwargs):
+        """
+        Save the user instance, ensuring the password is properly hashed.
+        """
+        super().save(*args, **kwargs)
+
+    @property
+    def is_anonymous(self):
+        """
+        Always return False. This is a way of comparing User objects to
+        anonymous users.
+        """
+        return False
+
+    @property
+    def is_authenticated(self):
+        """
+        Always return True. This is a way to tell if the user has been
+        authenticated in templates.
+        """
+        return True
--- config/settings.py
+++ config/settings.py
@@ -140,3 +140,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Register custom user model
+AUTH_USER_MODEL = 'accounts.User'
