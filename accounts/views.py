668a23984ec5f78df0e1e8bcfa80655cd5464699
suin park <tndls3026@ajou.ac.kr>
2024-10-13 16:04:27 +0900

    [feat/docs] write drf-spectacular document

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import logout
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample, OpenApiParameter
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -13,6 +15,24 @@ from core.permissions import IsNotAuthenticated
 class UserViewSet(viewsets.GenericViewSet):
     serializer_class = UserLogInPostSerializer
 
+    @extend_schema(
+        request=UserSignUpPostSerializer,
+        responses={
+            200: OpenApiResponse(
+                response=OpenApiTypes.OBJECT,
+                examples=[
+                    OpenApiExample(
+                        'Example Response',
+                        value={
+                            "access": "eyJhbGciOiJIUzI1NiIsInR5cCI...",
+                            "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI..."
+                        },
+                    )
+                ],
+            ),
+        },
+        description="회원가입 후 access와 refresh 토큰 반환",
+    )
     @action(methods=['POST'], detail=False, permission_classes=[IsNotAuthenticated])
     def signup(self, request: Request):
         serializer = UserSignUpPostSerializer(data=request.data)
@@ -26,6 +46,24 @@ class UserViewSet(viewsets.GenericViewSet):
 
         return Response(output_dto)
 
+    @extend_schema(
+        request=UserLogInPostSerializer,
+        responses={
+            200: OpenApiResponse(
+                response=OpenApiTypes.OBJECT,
+                examples=[
+                    OpenApiExample(
+                        'Example Response',
+                        value={
+                            "access": "eyJhbGciOiJIUzI1NiIsInR5cCI...",
+                            "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI..."
+                        },
+                    )
+                ],
+            ),
+        },
+        description="사용자 로그인 처리 및 JWT 토큰 반환",
+    )
     @action(methods=['POST'], detail=False, permission_classes=[IsNotAuthenticated])
     def login(self, request: Request):
         serializer = UserLogInPostSerializer(data=request.data)
@@ -39,6 +77,21 @@ class UserViewSet(viewsets.GenericViewSet):
 
         return Response(output_dto)
 
+    @extend_schema(
+        responses={
+            200: None,  # 로그아웃은 특별한 응답을 반환하지 않음
+        },
+        parameters=[
+            OpenApiParameter(
+                name='Authorization',
+                location=OpenApiParameter.HEADER,
+                description="JWT 인증 토큰",
+                required=True,
+                type=OpenApiTypes.STR
+            )
+        ],
+        description="사용자 로그아웃 처리",
+    )
     @action(methods=['POST'], detail=False, permission_classes=[IsAuthenticated])
     def logout(self, request: Request):
         logout(request)
--- todos/views.py
+++ todos/views.py
@@ -1,10 +1,13 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
 from todos.models import Todo
-from todos.serializers.todo_serializer import TodoCreatePostSerializer, TodoSerializer, TodoUpdatePostSerializer
+from todos.serializers.todo_serializer import TodoCreatePostSerializer, TodoSerializer, TodoUpdatePostSerializer, \
+    TodoRetrieveQsTodoSerializer, TodoListQsSerializer
 from todos.services.todo_service import TodoService
 
 
@@ -13,6 +16,22 @@ class TodoViewSet(viewsets.GenericViewSet):
     queryset = Todo.objects.all()
     permission_classes = [IsAuthenticated]
 
+    @extend_schema(
+        request=TodoCreatePostSerializer,
+        responses={
+            200: TodoRetrieveQsTodoSerializer,
+        },
+        parameters=[
+            OpenApiParameter(
+                name='Authorization',
+                location=OpenApiParameter.HEADER,
+                description="JWT 인증 토큰",
+                required=True,
+                type=OpenApiTypes.STR
+            )
+        ],
+        description="로그인된 사용자의 새로운 Todo 아이템 생성",
+    )
     def create(self, request: Request):
         serializer = TodoCreatePostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -22,12 +41,49 @@ class TodoViewSet(viewsets.GenericViewSet):
 
         return Response(output_dto)
 
+    @extend_schema(
+        responses={
+            200: TodoListQsSerializer(),
+        },
+        parameters=[
+            OpenApiParameter(
+                name='Authorization',
+                location=OpenApiParameter.HEADER,
+                description="JWT 인증 토큰",
+                required=True,
+                type=OpenApiTypes.STR
+            )
+        ],
+        description="로그인된 사용자의 Todo 목록 반환"
+    )
     def list(self, request: Request):
         service = TodoService(user=request.user)
         output_dto = service.list(request)
 
         return Response(output_dto)
 
+    @extend_schema(
+        responses={
+            200: TodoRetrieveQsTodoSerializer,
+        },
+        parameters=[
+            OpenApiParameter(
+                name='id',
+                location=OpenApiParameter.PATH,
+                description="조회할 Todo 아이템의 ID",
+                required=True,
+                type=OpenApiTypes.INT
+            ),
+            OpenApiParameter(
+                name='Authorization',
+                location=OpenApiParameter.HEADER,
+                description="JWT 인증 토큰",
+                required=True,
+                type=OpenApiTypes.STR
+            ),
+        ],
+        description="ID로 지정한 Todo 항목 반환"
+    )
     def retrieve(self, request: Request, pk):
         id = int(pk)
         service = TodoService(user=request.user)
@@ -35,6 +91,29 @@ class TodoViewSet(viewsets.GenericViewSet):
 
         return Response(output_dto)
 
+    @extend_schema(
+        request=TodoUpdatePostSerializer,
+        responses={
+            200: TodoRetrieveQsTodoSerializer,
+        },
+        parameters=[
+            OpenApiParameter(
+                name='id',
+                location=OpenApiParameter.PATH,
+                description="수정할 Todo 아이템의 ID",
+                required=True,
+                type=OpenApiTypes.INT
+            ),
+            OpenApiParameter(
+                name='Authorization',
+                location=OpenApiParameter.HEADER,
+                description="JWT 인증 토큰",
+                required=True,
+                type=OpenApiTypes.STR
+            ),
+        ],
+        description="ID로 지정한 Todo 항목 내용 업데이트"
+    )
     def update(self, request: Request, pk):
         id = int(pk)
         serializer = TodoUpdatePostSerializer(data=request.data)
@@ -45,7 +124,29 @@ class TodoViewSet(viewsets.GenericViewSet):
 
         return Response(output_dto)
 
-    def delete(self, request: Request, pk):
+    @extend_schema(
+        responses={
+            200: None,
+        },
+        parameters=[
+            OpenApiParameter(
+                name='id',
+                location=OpenApiParameter.PATH,
+                description="삭제할 Todo 아이템의 ID",
+                required=True,
+                type=OpenApiTypes.INT
+            ),
+            OpenApiParameter(
+                name='Authorization',
+                location=OpenApiParameter.HEADER,
+                description="JWT 인증 토큰",
+                required=True,
+                type=OpenApiTypes.STR
+            ),
+        ],
+        description="ID로 지정한 Todo 항목 삭제"
+    )
+    def destroy(self, request: Request, pk):
         id = int(pk)
         service = TodoService(user=request.user)
         service.delete(id=id)
